b2971d57958475aea7a70dbd20966928d7d0d1b7
Tom Dooner <tom.dooner@causes.com>
2014-04-24 22:32:24 -0700

    Fix recipient_id validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :name, presence: true
   # validates :access_token, presence: true
-  validates :registration_id, presence: true
+  validates :recipient_id, presence: true
 end
