54f8cf06eb554cfb22e0dab60a201dfa55ccd70f
Tom Dooner <tom.dooner@causes.com>
2014-04-24 22:34:13 -0700

    Remove name validation for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   validates :email, presence: true
-  validates :name, presence: true
+  # validates :name, presence: true
   # validates :access_token, presence: true
   validates :recipient_id, presence: true
 end
